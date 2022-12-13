6235733f7d7e56726afb37284d4e91834f514f93
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2022-12-13 12:16:32 -0300

    Script de comparação das simulações

---
+++ simulations-comparison.sql
@@ -0,0 +1,50 @@
+SELECT
+	*,
+	(S.dias_prejuizo::FLOAT / S.total_dias * 100) AS percentual_dias_prejuizo,
+	(S.dias_lucro::FLOAT / S.total_dias * 100) AS percentual_dias_lucro,
+	(S.dias_lucro::FLOAT / S.dias_prejuizo) AS dias_de_lucro_por_dia_de_prejuizo
+FROM (
+	SELECT
+		'simulacao_top_3' AS tabela,
+		MIN(resultado_total) AS maior_prejuizo,
+		AVG(resultado_total) AS resultado_medio,
+		MAX(resultado_total) AS maior_lucro,
+		COUNT(*) AS total_dias,
+		SUM(CASE WHEN resultado_total <= 1 THEN 1 ELSE 0 END) AS dias_prejuizo,
+		SUM(CASE WHEN resultado_total > 1 THEN 1 ELSE 0 END) AS dias_lucro,
+		SUM((100 * resultado_total) - 100) AS retorno_investimento
+	FROM simulacao_top_3
+	UNION ALL
+	SELECT
+		'simulacao_top_5' AS tabela,
+		MIN(resultado_total) AS maior_prejuizo,
+		AVG(resultado_total) AS resultado_medio,
+		MAX(resultado_total) AS maior_lucro,
+		COUNT(*) AS total_dias,
+		SUM(CASE WHEN resultado_total <= 1 THEN 1 ELSE 0 END) AS dias_prejuizo,
+		SUM(CASE WHEN resultado_total > 1 THEN 1 ELSE 0 END) AS dias_lucro,
+		SUM((100 * resultado_total) - 100) AS retorno_investimento
+	FROM simulacao_top_5
+	UNION ALL
+	SELECT
+		'simulacao_top_7' AS tabela,
+		MIN(resultado_total) AS maior_prejuizo,
+		AVG(resultado_total) AS resultado_medio,
+		MAX(resultado_total) AS maior_lucro,
+		COUNT(*) AS total_dias,
+		SUM(CASE WHEN resultado_total <= 1 THEN 1 ELSE 0 END) AS dias_prejuizo,
+		SUM(CASE WHEN resultado_total > 1 THEN 1 ELSE 0 END) AS dias_lucro,
+		SUM((100 * resultado_total) - 100) AS retorno_investimento
+	FROM simulacao_top_7
+	UNION ALL
+	SELECT
+		'simulacao_top_10' AS tabela,
+		MIN(resultado_total) AS maior_prejuizo,
+		AVG(resultado_total) AS resultado_medio,
+		MAX(resultado_total) AS maior_lucro,
+		COUNT(*) AS total_dias,
+		SUM(CASE WHEN resultado_total <= 1 THEN 1 ELSE 0 END) AS dias_prejuizo,
+		SUM(CASE WHEN resultado_total > 1 THEN 1 ELSE 0 END) AS dias_lucro,
+		SUM((100 * resultado_total) - 100) AS retorno_investimento
+	FROM simulacao_top_10
+) S;
